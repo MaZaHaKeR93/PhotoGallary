52d9259d15987083ca4c5ec945630647b12ecfda
Andrei Nishanov <mazahaker93@gmail.com>
2017-10-28 12:51:18 +0600

    Add seeds for users&pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 admin = User.create!(name: 'Admin', email: 'admin@admin.ru', password: 'qweqweqwe', admin: true)
 user = User.create!(name: 'User', email: 'user@user.ru', password: 'qweqweqwe')
+
+10.times do 
+	new_user = User.create!(name: Faker::Name.name, email: Faker::Internet.unique.email, password: 'qweqweqwe')
+	
+	5.times do
+		new_picture = Picture.create!(title: Faker::Company.name, 
+																	image: File.new("#{Rails.root}/public/uploads/default/big_default.jpg"),
+																	user_id: new_user.id)
+	end
+end
