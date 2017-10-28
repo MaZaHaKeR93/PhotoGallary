26887876306af7457379b5be5f1620f24a000e98
Andrei Nishanov <mazahaker93@gmail.com>
2017-10-28 14:17:30 +0600

    Add validates for picture fields, add show view for picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,7 @@
 class Picture < ActiveRecord::Base
   belongs_to :user
   mount_uploader :image, ImageUploader
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :image, presence: true
 end
